edf33b090e9276f003df4cfb35c0bc16d734c818
DevHola <Connectola@yahoo.com>
2022-03-28 02:29:42 -0700

    'Fix-ui'

--- src/screens/Ascreens/Navbars.tsx
+++ src/screens/Ascreens/Navbars.tsx
@@ -1,24 +1,46 @@
-import React from 'react'
-import {Navbar,Container,Nav} from 'react-bootstrap'
-
-export default function Navbars() {
-  const logout = ()=>{
+import React, { useState } from "react";
+import { Navbar, Container } from "react-bootstrap";
+import "./Navbarse.css";
+export default function Navbars(props) {
+  const [isactive, setactive] = useState(false);
+  const logout = () => {
     sessionStorage.removeItem("Auth Token");
     window.location.reload();
-  }
+  };
+  const open = (e) => {
+    e.preventDefault();
+    setactive(true);
+    props.data(true);
+  };
+  const close = (e) => {
+    e.preventDefault();
+    setactive(false);
+    props.data(false);
+  };
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">Trackdefi</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-   
-    <Nav>
+      <Container>
+        <Navbar.Brand href="#home">Trackdefi</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end m-2">
+          {isactive === false && (
+            <button className="btn btn-success breaker" onClick={open}>
+              Set Wallet
+            </button>
+          )}
+          {isactive === true && (
+            <button className="btn btn-primary breaker m-1" onClick={close}>
+              Close Dialog
+            </button>
+          )}
 
-        <Nav.Link  onClick={logout}>Log Out</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-  )
+          <hr></hr>
+
+          <button className="btn btn-danger m-1" onClick={logout}>
+            logout
+          </button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
--- src/screens/Footer.tsx
+++ src/screens/Footer.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 export default function Footer() {
   return (
-    <footer className='container-fluid footer'>
-      <hr ></hr>
-     <div className='row'>
-     <div className='col-sm'>
-       <p className='text-center'>&copy; 2021 Trackdefi All right reserved</p>
-     </div>
-     </div>
+    <footer className="container-fluid footer">
+      <hr></hr>
+      <div className="row">
+        <div className="col-sm">
+          <p className="text-center">
+            &copy; 2021 Trackdefi All right reserved
+          </p>
+        </div>
+      </div>
     </footer>
-  )
+  );
 }
